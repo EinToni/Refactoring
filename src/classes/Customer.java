eb1a2ee6916a133a1b4ad6815e0b6e8aa0fe75a5
Toni <toni.einecker@gmail.com>
2020-05-13 18:39:43 +0200

    Replace Conditional with Polymorphism

--- src/classes/Customer.java
+++ src/classes/Customer.java
@@ -16,10 +16,21 @@ public class Customer {
         return name;
     };
     public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = (Rental) enum_rentals.nextElement();
+            //determine amounts for each line
+            // add frequent renter points
+            frequentRenterPoints += each.getFrequentRenterPoints();
+            //show figures for this rental
+            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
+        }
         //add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
--- src/classes/NewReleasePrice.java
+++ src/classes/NewReleasePrice.java
@@ -4,4 +4,10 @@ public class NewReleasePrice extends Price {
 	int getPriceCode() {
 		return Movie.NEW_RELEASE;
 	}
+	public double getCharge(int daysRented) {
+		return daysRented * 3;
+	}
+	int getFrequentRenterPoints(int daysRented) {
+		return (daysRented > 1) ? 2 : 1;
+	}
 }
